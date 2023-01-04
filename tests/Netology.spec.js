20093199676d700c145be9b62bdf782dec474936
JaneEyre333 <nasgur@mail.ru>
2023-01-04 12:06:49 +0300

    first commit

---
+++ tests/Netology.spec.js
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+import { login, password } from '../user';
+
+const { chromium } = require("playwright");
+
+test('test', async () => {
+  const browser = await chromium.launch({
+    headless: false,
+    slowMo: 1000,
+    devtools: false
+  });
+  const page = await browser.newPage();
+  await page.goto('https://netology.ru/');
+  await page.getByRole('link', { name: 'Войти' }).click();
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill(login);
+  await page.getByPlaceholder('Пароль').click();
+  await page.getByPlaceholder('Пароль').fill(password);
+  await page.getByTestId('login-submit-btn').click();
+  await expect(page.getByTestId('menu-userface')).toBeVisible();
+});
