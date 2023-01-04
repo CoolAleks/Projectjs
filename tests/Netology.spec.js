732b7701c2c9735e7a7a4cb36730dc27039f5970
JaneEyre333 <nasgur@mail.ru>
2023-01-04 12:21:10 +0300

    test2 added

--- tests/Netology.spec.js
+++ tests/Netology.spec.js
@@ -3,7 +3,7 @@ import { login, password } from '../user';
 
 const { chromium } = require("playwright");
 
-test('test', async () => {
+test('test1', async () => {
   const browser = await chromium.launch({
     headless: false,
     slowMo: 1000,
@@ -18,4 +18,21 @@ test('test', async () => {
   await page.getByPlaceholder('Пароль').fill(password);
   await page.getByTestId('login-submit-btn').click();
   await expect(page.getByTestId('menu-userface')).toBeVisible();
+});
+
+test('test2', async () => {
+  const browser = await chromium.launch({
+    headless: false,
+    slowMo: 1000,
+    devtools: false
+  });
+  const page = await browser.newPage();
+  await page.goto('https://netology.ru/');
+  await page.getByRole('link', { name: 'Войти' }).click();
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill(login);
+  await page.getByPlaceholder('Пароль').click();
+  await page.getByPlaceholder('Пароль').fill("gfhf");
+  await page.getByTestId('login-submit-btn').click();
+  await expect(page.getByTestId('login-error-hint')).toBeVisible();
 });
